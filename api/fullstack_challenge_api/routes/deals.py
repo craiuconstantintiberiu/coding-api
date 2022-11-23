992283ed427c33ec46839bb39ffdbcb80e1a7be5
ttibi <craiu.constantin.tiberiu@gmail.com>
2022-11-23 15:41:41 +0200

    Add simple routes to get all info

--- api/fullstack_challenge_api/routes/deals.py
+++ api/fullstack_challenge_api/routes/deals.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from ..models.Company import Company
+from ..models.Deal import Deal
 from ..utils.db import get_db
 
 router = APIRouter()
 
 
-@router.get("/companies")
+@router.get("/deals")
 async def get_companies(db: Session = Depends(get_db)):
-    return db.query(Company).all()
+    return db.query(Deal).all()
